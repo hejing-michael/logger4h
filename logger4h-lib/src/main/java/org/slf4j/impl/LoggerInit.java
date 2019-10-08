2d7cd217c87a5c2e3bd7e260d7f8eef1633ac06d
hejing <hejing.michael@qq.com>
2019-10-08 17:56:37 +0800

    修改log

--- logger4h-lib/src/main/java/org/slf4j/impl/LoggerInit.java
+++ logger4h-lib/src/main/java/org/slf4j/impl/LoggerInit.java
@@ -8,12 +8,24 @@ import android.os.Build;
 import org.slf4j.impl.logger.AndroidLoggerFactory;
 import org.slf4j.impl.utils.FileOutTimeUtils;
 
-import java.io.File;
-
 public class LoggerInit {
     private static AndroidLoggerFactory mAndroidLoggerFactory;
 
-    public static void init(Context context, File log) {
+    public static void init(Context context, String logDir) {
+        FileOutTimeUtils.makeDirs(logDir);
+        init(context, new AndroidLoggerFactory.Builder()
+                .setMaxSaveDay(1)
+                .setBufferSize(1024 * 2)
+                .setBufferDirPath(logDir)
+                .setLogDirPath(logDir)
+                .setPattern("yyyy-MM-dd_HH")
+                .setSuffix(".log")
+                .setCompress(false)
+                .clearAllOutOfDateFiles()
+                .create());
+    }
+
+    public static void init(Context context, AndroidLoggerFactory androidLoggerFactory) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (context.checkSelfPermission(Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED
                     || context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
@@ -22,20 +34,8 @@ public class LoggerInit {
             }
         }
 
-        String logPath = log.getAbsolutePath();
-
-        FileOutTimeUtils.makeDirs(logPath);
-        mAndroidLoggerFactory = new AndroidLoggerFactory.Builder()
-                .setMaxSaveDay(1)
-                .setBufferSize(1024 * 2)
-                .setBufferDirPath(logPath)
-                .setLogDirPath(logPath)
-                .setPattern("yyyy-MM-dd_HH")
-                .setSuffix(".log")
-                .setCompress(false)
-                .clearAllOutOfDateFiles()
-                .create();
-        StaticLoggerBinder.getSingleton().setLoggerFactory(mAndroidLoggerFactory);
+        mAndroidLoggerFactory = androidLoggerFactory;
+        StaticLoggerBinder.getSingleton().setLoggerFactory(androidLoggerFactory);
     }
 
 
